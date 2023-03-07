07427b86f17960baaa8ecdea4394d3c05a571a4e
LAPTOP-FGFQK50O\LENOVO <ninkaishou27@gmail.com>
2023-03-07 23:05:09 +0700

    login screen done

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:buka_usaha/utils/const.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -21,28 +22,223 @@ class _LoginScreenState extends State<LoginScreen> {
               left: 16,
             ),
             child: Container(
-              color: Colors.red,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
+              height: 150,
+              child: Stack(
                 children: [
-                  Container(
+                  Positioned(
+                    right: 0,
+                    top: 0,
+                    child: Image.asset(
+                      'assets/image/loginLogo.png',
+                      scale: 5,
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    top: 70,
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('Login')
+                        Text(
+                          'Login',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Masukkan email dan password anda',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
                       ],
                     ),
                   ),
-                  Expanded(
-                    child: Container(),
-                  ),
-                  Image.asset(
-                    'assets/image/loginLogo.png',
-                    scale: 4,
-                  )
                 ],
               ),
             ),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 32, right: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Email/Username',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7),
+                    border: Border.all(
+                      color: grey,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: TextFormField(
+                      textAlignVertical: TextAlignVertical.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Email/Username',
+                      ),
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: grey,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 32, right: 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Password',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(7),
+                    border: Border.all(
+                      color: grey,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: TextField(
+                      textAlignVertical: TextAlignVertical.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Password',
+                      ),
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: grey,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      Expanded(child: Container()),
+                      InkWell(
+                        onTap: () {},
+                        child: Text(
+                          'Lupa Password?',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 8,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 32, right: 32),
+            child: GestureDetector(
+              onTap: () {},
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromRGBO(83, 211, 255, 1),
+                      Color.fromRGBO(0, 162, 247, 1),
+                      Color.fromRGBO(111, 86, 211, 1),
+                    ],
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                  ),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(top: 5, bottom: 5),
+                  child: Center(
+                    child: Text(
+                      'Masuk',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 12,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 70,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 32, right: 32),
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                'Belum punya akun? Buat akun',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
           ),
         ],
       ),
--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -1,1 +1,3 @@
 import 'package:flutter/animation.dart';
+
+const Color grey = Color.fromRGBO(131, 131, 131, 1);
