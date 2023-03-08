66d6aa9dca7ac0e1334d624de88b0a1d1b3feddd
LAPTOP-FGFQK50O\LENOVO <ninkaishou27@gmail.com>
2023-03-08 14:49:03 +0700

    profile screen done

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,5 +1,6 @@
 import 'package:buka_usaha/component/styleText.dart';
 import 'package:buka_usaha/utils/const.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -35,11 +36,12 @@ class _ProfileState extends State<Profile> {
                       color: Colors.black,
                     ),
                     IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.settings_outlined,
-                          color: Colors.black,
-                        ))
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.settings_outlined,
+                        color: Colors.black,
+                      ),
+                    )
                   ],
                 ),
                 Row(
@@ -57,7 +59,7 @@ class _ProfileState extends State<Profile> {
                       fontSize: 16,
                       fontWeight: FontWeight.w600,
                       color: Colors.black,
-                    )
+                    ),
                   ],
                 )
               ],
@@ -65,6 +67,117 @@ class _ProfileState extends State<Profile> {
           ),
         ),
         preferredSize: Size.fromHeight(100),
+      ),
+      body: ListView(
+        children: [
+          SizedBox(
+            height: 12,
+          ),
+          Container(
+            width: double.infinity,
+            height: 48,
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+                right: 16,
+                bottom: 12,
+                top: 12,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/image/fotoProfil.png',
+                    scale: 4,
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  StyleText(
+                    data: 'Apa yang anda pikirkan?',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: abu,
+                  ),
+                  Expanded(child: Container()),
+                  Image.asset(
+                    'assets/image/iconProfile.png',
+                    scale: 1,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Container(
+            color: Colors.white,
+            width: double.infinity,
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/image/fotoProfil.png',
+                          scale: 5,
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        StyleText(
+                          data: 'anda',
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  StyleText(
+                    data: 'Postingan pertama saya di komunitas',
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Container(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.favorite_outline,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.chat_outlined,
+                          color: Colors.black,
+                        ),
+                        Expanded(child: Container()),
+                        Icon(
+                          Icons.bookmark_outline_rounded,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
